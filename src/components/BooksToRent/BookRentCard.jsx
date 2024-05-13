e747fc3ec3cf1abe6345ce5574c62a89f0aeecd1
MD ALAMIN KAZI <kmalamin123210@gmail.com>
2024-05-14 03:30:36 +0600

    added footer

--- src/components/BooksToRent/BookRentCard.jsx
+++ src/components/BooksToRent/BookRentCard.jsx
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const BookRentCard = ({ book }) => {
   const { image, name, _id } = book;
   return (
-    <div className="card w-[280px] h-[350px] bg-base-100 shadow-xl border-2 p-6 mr-10  border-b-[#cab956] border-l-[#cab956]">
-      <figure className="px-10 pt-10">
+    <div className="card w-[280px] h-[350px] bg-base-100 shadow-xl border-2 p-4 mr-10  border-b-[#cab956] border-l-[#cab956]">
+      <figure className="px-10 pt-10 transition-transform duration-300 transform hover:scale-110">
         <img src={image} alt="Shoes" className="rounded-xl" />
       </figure>
       <div className="card-body items-center text-center">
